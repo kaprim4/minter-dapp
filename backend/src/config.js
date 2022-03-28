4924410a3cdbba42cac9bfc46c56d8336ae65d05
kaprim4 <87yahya@gmail.com>
2022-03-28 23:04:38 +0000

    change

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const PUBLIC_MINT_START_DATE = "2022-03-28T23:00:00+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-03-28T00:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0xfaF01AD252D2d6350b8ebA6e3f6329779E14AafD"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = ['0xfaF01AD252D2d6350b8ebA6e3f6329779E14AafD']; // only update if you want to manually set the whitelisted addresses
